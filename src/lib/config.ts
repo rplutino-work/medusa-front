c2830e428f536037f758987289fb8778df584a0a
Rodrigo <rodrigo@pow.la>
2022-11-23 22:22:21 -0300

    first commit

---
+++ src/lib/config.ts
@@ -0,0 +1,23 @@
+import Medusa from "@medusajs/medusa-js"
+import { QueryClient } from "react-query"
+
+// Defaults to standard port for Medusa server
+let MEDUSA_BACKEND_URL = " https://medusa-server-01.herokuapp.com"
+
+if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 60 * 24,
+      retry: 1,
+    },
+  },
+})
+
+const medusaClient = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+
+export { MEDUSA_BACKEND_URL, queryClient, medusaClient }
